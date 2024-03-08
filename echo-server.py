d6d278b70645a360de5c6949a63fcf3be9e9d3ba
ARCT_SAE <masonvalles04@gmail.com>
2024-03-08 11:57:19 -0500

    merging AI code

--- echo-server.py
+++ echo-server.py
@@ -9,16 +9,9 @@ import serial
 
 from ublox_gps import UbloxGps
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65430  # Port to listen on (non-privileged ports are > 1023)
+HOST = "0.0.0.0"  # Standard loopback interface address (localhost)
+PORT = 65438  # Port to listen on (non-privileged ports are > 1023)
 BASE_STATION_ADDRESS = "10.0.0.2"  # U-center NTRIP server address
-
-# initiate NTRIP connection with base station
-command = ["str2str", "-in", "ntrip://sae:1234@" +
-           BASE_STATION_ADDRESS + ":2101/ARCT"]
-serial_device = open['/dev/ttys0', 'w']
-process = subprocess.Popen(
-    command, stdout=serial_device, stderr=subprocess.PIPE, text=True)
 
 # establish connection with GPS module stream
 serial_port = serial.Serial('/dev/ttyS0', baudrate=38400, timeout=1)
@@ -52,8 +45,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:  # initialize TCP S
                     longitude = geo.lon
 
                     # LOAD GPS DATA HERE
-                   # latitude = 33.837189
-                   # longitude = -84.53877
+                    # latitude = 33.837189
+                    # longitude = -84.53877
 
                     # Create response with coordinates and pixels
                     response = {
@@ -72,15 +65,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:  # initialize TCP S
                     conn.sendall(string_json.encode('utf-8'))
 
                 case "primary_status":
-                    # Get coordinates
-                    # geo = gps.geo_coords()
-                    # latitude = geo.lat
-                    # longitude = geo.lon
-
                     # PUT GPS DATA HERE
-                    time.sleep(1)
-                    latitude = round(random.uniform(-90, 91), 6)
-                    longitude = round(random.uniform(-180, 181), 6)
+                    geo = gps.geo_coords()
+                    latitude = geo.lat
+                    longitude = geo.lon
 
                     # Create response
                     response = {
@@ -96,8 +84,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:  # initialize TCP S
                     # Send the response
                     conn.sendall(response_dump_string.encode('utf-8'))
 
-                case "target_found":
+                # case "target_found":
                     # send target coordinates to pada (port stuff)
                     # ...
                     # ...
-                    time.sleep(1)
+                    # time.sleep(1)
